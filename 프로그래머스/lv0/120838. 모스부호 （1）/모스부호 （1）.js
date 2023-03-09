a9cc9c26ea7e6eb5f4656cea7299ab6069e69a15
junyup0503 <97156898+junyup0503@users.noreply.github.com>
2023-03-09 16:20:25 +0900

    [level 0] Title: 모스부호 (1), Time: 0.21 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,15 @@
+function solution(letter) {
+    const morse = { '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+                    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+                    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+                    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+                    '-.--':'y','--..':'z'
+    };
+    let answer = "";
+    letter.split(" ").forEach(word => {
+        word.split("/").forEach(code => {
+            answer += morse[code];
+        });
+    });
+    return answer;
+}
