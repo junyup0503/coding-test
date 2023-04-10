2a803828deb23932ade242fba02db1ced5fcdca3
junyup0503 <97156898+junyup0503@users.noreply.github.com>
2023-04-10 18:09:14 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,12 +1,4 @@
 function solution(numbers) {
-    let maxProduct = -Infinity;
-
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      const product = numbers[i] * numbers[j];
-      maxProduct = Math.max(maxProduct, product);
-    }
-  }
-
-  return maxProduct;
+    numbers.sort((a,b)=>a-b);
+  return numbers[numbers.length-1] * numbers[numbers.length -2];
 }
