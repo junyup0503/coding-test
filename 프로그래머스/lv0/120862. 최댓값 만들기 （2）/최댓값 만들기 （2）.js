3aacb70f0b84cac9fc69b3d9f1b182b79d087699
junyup0503 <97156898+junyup0503@users.noreply.github.com>
2023-04-10 18:10:55 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,21 +1,5 @@
 function solution(numbers) {
-    let answer = Number.MIN_SAFE_INTEGER;
-    let ch = Array.from({length: numbers.length + 1}, () => 0);
+    numbers.sort((a,b) => a-b);
     
-    function DFS(L, sum) {
-        if(L === 2) return answer = Math.max(answer, sum);
-        else {
-            for(let i = 0; i < numbers.length; i++) {
-                if(ch[i] === 0) {
-                    ch[i] = 1;
-                    DFS(L+1, sum * numbers[i]);
-                    ch[i] = 0;    
-                }
-            }
-        }
-    }
-    
-    DFS(0, 1);
-    
-    return answer;
+    return Math.max(numbers[0]*numbers[1], numbers[numbers.length - 1]*numbers[numbers.length - 2]);
 }
