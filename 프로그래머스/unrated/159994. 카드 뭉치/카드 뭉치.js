5be6d19eb5d41c98382d49838d34714b470ede2f
junyup0503 <97156898+junyup0503@users.noreply.github.com>
2023-05-12 11:30:29 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,16 @@
+function solution(cards1, cards2, goal) {
+    let answer = "Yes";
+
+  for (let i of goal) {
+    if (i === cards1[0]) {
+      cards1.shift();
+    } else if (i === cards2[0]) {
+      cards2.shift();
+    } else {
+      answer = "No";
+      break;
+    }
+  }
+
+  return answer;
+}
